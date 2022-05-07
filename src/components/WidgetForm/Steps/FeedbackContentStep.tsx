d56a1c8d4981bfc703ec5aade3fbabd6707f1156
leafarpontes <rafapontes2008@hotmail.com>
2022-05-07 12:10:43 -0300

    Adicionado funcionalidade de tirar screenshot

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -11,7 +13,9 @@ export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested
 }: FeedbackContentStepProps) {
-  const feedbackTypeInfo = feedbackTypes[feedbackType]
+  const [screenshot, setScreenshot] = useState<string | null>('');
+
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   return (
     <>
@@ -37,14 +41,26 @@ export function FeedbackContentStep({
 
       <form className="my-4 w-full">
         <textarea
-          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          className="min-w-[304px] w-full min-h-[112px] text-sm 
+          placeholder-zinc-400 text-zinc-100 border-zinc-600 
+          bg-transparent rounded-md focus:border-brand-500 
+          focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none 
+          scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
         />
 
         <footer className="flex gap-2 mt-2">
+          <ScreenshotButton 
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
+
           <button
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 
+            justify-center items-center text-sm hover:bg-brand-300 
+            focus:outline-none focus:ring-2 focus:ring-offset-2 
+            focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
           >
             Enviar feedback
           </button>
